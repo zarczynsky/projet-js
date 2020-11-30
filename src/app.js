050324c552d2c8416fb8b6d5caf203ceb50893cc
Jakub Szumilas <szumilas.jakub@gmail.com>
2020-12-01 00:25:43 +0100

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello, Mr. World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
+
+app.get('/rzodkiew', (req,res) => {
+    res.send('Something called rzodkiew has been requested. Sadly, there is no rzodkiew here.') // TODO: Make it a 404
+})
