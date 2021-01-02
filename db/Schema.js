3f1de466de7419fc4218bc8b4294f8a4a70f9a4d
zarczynsky <54021935+zarczynsky@users.noreply.github.com>
2021-01-02 01:12:59 +0100

    find dish

--- db/Schema.js
+++ db/Schema.js
@@ -1,15 +1,48 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const dishSchema = new mongoose.Schema({
     name: {type: String, unique:true},
 //    some_id: {type: ObjectId},
-    ingredients: {type: Array},
-    time: {type: Number}
+//     ingredients: {type: Array},
+//     time: {type: Number},
+//     text: {type: String},
+//     likes: {type: Number}
 });
 
-const userSchema = new mongoose.Schema({
-    name: {type: String, unique:true, required:true},
-//    some_id: {type: ObjectId},
-    password: {type: String, required:true},
-});
+dishSchema.methods = {
+    user_view() {
+        return {
+            name: this.name
+            // ingredients: this.ingredients,
+            // text: this.text,
+            // time: this.time,
+            // likes: this.likes
+        }
+    }
+}
+
+// dishSchema.methods.user_view = function (){
+//     return {
+//         name: this.name
+//         // ingredients: this.ingredients,
+//         // text: this.text,
+//         // time: this.time,
+//         // likes: this.likes
+//     }
+// }
+
+//dishSchema.methods.findSimilarTypes = function(cb) {
+//    return mongoose.model('Animal').find({ name: this.name }, cb);
+//};
+
+let mongo = mongoose.model('Dish', dishSchema);
+module.exports = mongo
+
+// const userSchema = new mongoose.Schema({
+//     name: {type: String, unique:true, required:true},
+// //    some_id: {type: ObjectId},
+//     password: {type: String, required:true},
+// });
+//
+// module.exports = mongoose.model('User', userSchema);
+
--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+
+const Dish = require('../db/Schema')
+const asyncHandler = require("../db/async-handler");
+
+//import require data
+const dishRouter = require('./dish.controller');
+
 app.get('/', (req, res) => {
   res.send('Hello, Mr. World!')
 })
@@ -17,3 +27,57 @@ app.get('/rzodkiew', (req,res) => {
 app.get('/buraki', (req,res) => {
     res.send('Something called buraki has been requested. Sadly, there is no buraki here.')
 })// TODO: Make it a 404
+
+
+
+// const express = require('express')
+// const app = express()
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send(
+        {
+            message: err.message || 'Error occurred'
+        }
+    )
+}
+
+// Dish.find({ 'name': 'Ale5sss5' }, 'name age', function (err, athletes) {
+//     if (err) return errorHandler(err);
+//     // 'athletes' contains the list of athletes that match the criteria.
+// })
+//
+// const ai = new Dish({name:'Ale5sss5'});
+// ai.save(function (err) {
+//     if (err) return errorHandler(err);
+//     // saved!
+// });
+
+// POST /api/dish
+app.post('/dish', asyncHandler(async (req, res) => {
+    const body = req.body;
+    const dish = await Dish.create(body);
+    res.json(dish.view());
+}))
+
+// app.get('/show', (req,res) => {
+//     const query = Dish.findOne({ name: 'Ale5sss5'},  function (err, athletes) {
+//         if (err) console.log(err);
+//         // 'athletes' contains the list of athletes that match the criteria.
+//     })
+//     console.log(typeof query)
+//     res.json(query.user_view())
+// })
+
+app.route("/find").get(function(req, res) {
+    Dish.find({ name: 'Ale5sss5' }, function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+
+
