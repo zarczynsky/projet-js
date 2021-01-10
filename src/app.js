14acb33b4c9e61f896123db4c54847a01c7f5344
szumied <szumilas.jakub@gmail.com>
2021-01-10 04:49:06 +0100

    Formatting check

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 mongoose.set('useCreateIndex', true);
 
 const cookieParser = require('cookie-parser')
@@ -11,12 +14,12 @@ const config = require('./config')
 
 const Dish = require('./schema/dishSchema')
 const User = require('./schema/userSchema')
-const asyncHandler =  require("./api/async-handler")
-const {sign} = require('./services/jwt')
+const asyncHandler = require("./api/async-handler")
+const {
+    sign
+} = require('./services/jwt')
 
-const User=require('./schema/userSchema')
-
-
+const User = require('./schema/userSchema')
 
 const argon2 = require('argon2');
 app.use(cookieParser(config.cookiesSecret))
@@ -26,11 +29,9 @@ const UnauthorizedException = require("./exceptions/unauthorized-exception");
 
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
-    res.status(err.status || 500).send(
-        {
-            message: err.message || 'Error occurred'
-        }
-    )
+    res.status(err.status || 500).send({
+        message: err.message || 'Error occurred'
+    })
 }
 
 //import require data
@@ -38,23 +39,25 @@ const errorHandler = (err, req, res, next) => {
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
 
 app.get('/', (req, res) => {
-  res.send('Hello, Mr. World!')
+    res.send('Hello, Mr. World!')
 })
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
 
-app.get('/rzodkiew', (req,res) => {
+app.get('/rzodkiew', (req, res) => {
     res.send('Something called rzodkiew has been requested. Sadly, there is no rzodkiew here.') // TODO: Make it a 404
 })
 
-app.get('/buraki', (req,res) => {
+app.get('/buraki', (req, res) => {
     res.send('Something called buraki has been requested. Sadly, there is no buraki here.')
-})// TODO: Make it a 404
+}) // TODO: Make it a 404
 
 // const ai = new Dish({name:'Ale5sss5'});
 // ai.save(function (err) {
@@ -72,7 +75,9 @@ const dish5 = new Dish({name:'tort', ingredients:['truskawki', 'proszek'], time:
 
 app.post('/find/name', asyncHandler(async (req, res) => {
     const name = req.body.name;
-    Dish.find({ name: name }, function(err, result) {
+    Dish.find({
+        name: name
+    }, function (err, result) {
         if (err) {
             console.log(err);
         } else {
@@ -90,7 +95,9 @@ app.post('/find/ingredients', asyncHandler(async (req, res) => {
     // }
     //
     // console.log(authHeader)
-    Dish.find({ ingredients: name }, function(err, result) {
+    Dish.find({
+        ingredients: name
+    }, function (err, result) {
         if (err) {
             console.log(err);
         } else {
@@ -101,11 +108,11 @@ app.post('/find/ingredients', asyncHandler(async (req, res) => {
 }))
 
 app.post('/add', asyncHandler(async (req, res) => {
-    const name=req.body.name;
-    const ingredients=req.body.ingredients;
-    const time=req.body.times;
-    const recipe=req.body.recipe;
-    const likes=req.body.likes;
+    const name = req.body.name;
+    const ingredients = req.body.ingredients;
+    const time = req.body.times;
+    const recipe = req.body.recipe;
+    const likes = req.body.likes;
 
     // const authHeader = req.headers.authorization;
     // if(!authHeader){
@@ -118,37 +125,54 @@ app.post('/add', asyncHandler(async (req, res) => {
     // const [email, password] = Buffer.from(b64auth, 'base64').toString().split(':')
 
 
-    const ai = new Dish({name: name, ingredients: ingredients, time: time, text: recipe, likes:likes});
+    const ai = new Dish({
+        name: name,
+        ingredients: ingredients,
+        time: time,
+        text: recipe,
+        likes: likes
+    });
     await ai.save(function (err) {
-    if (err) console.log(err);
+        if (err) console.log(err);
 
 
-    // saved!
-});
-    res.json({status: "Przepis dodany"})
+        // saved!
+    });
+    res.json({
+        status: "Przepis dodany"
+    })
 }))
 
 app.post('/createaccount', asyncHandler(async (req, res) => {
-    const name=req.body.name;
-    const password=req.body.password;
+    const name = req.body.name;
+    const password = req.body.password;
     try {
         const hash = await argon2.hash(password);
-        const user=new User({name: name, password: hash});
+        const user = new User({
+            name: name,
+            password: hash
+        });
         await user.save(function (err) {
             if (err) console.log(err);
             // saved!
         });
-        res.json({status: "Konto utworzone"})
+        res.json({
+            status: "Konto utworzone"
+        })
     } catch (err) {
-        res.json({status: "error"})
+        res.json({
+            status: "error"
+        })
     }
 
 }))
 
 app.post('/login', asyncHandler(async (req, res) => {
-    const name=req.body.name;
-    const password=req.body.password;
-    const hashedpassword = await User.findOne({name: name});
+    const name = req.body.name;
+    const password = req.body.password;
+    const hashedpassword = await User.findOne({
+        name: name
+    });
 
     try {
         if (await argon2.verify(hashedpassword['password'], password)) {
@@ -156,17 +180,25 @@ app.post('/login', asyncHandler(async (req, res) => {
             const token = sign(name);
             res.cookie('auth', token, config.cookiesOptions)
 
-            return res.json({status: "log in"})
+            return res.json({
+                status: "log in"
+            })
         } else {
-            res.json({status: "bad password"})
+            res.json({
+                status: "bad password"
+            })
         }
     } catch (err) {
-        res.json({status: "no user"})
+        res.json({
+            status: "no user"
+        })
     }
 }))
 
 const undefinedEndpointHandler = (req, res) => {
-    res.status(404).send({error: 'Undefined Endpoint'})
+    res.status(404).send({
+        error: 'Undefined Endpoint'
+    })
 }
 
 app.use(undefinedEndpointHandler)
