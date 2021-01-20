90bf49443d2b10cb0258817380653a8189cf5dd0
zarczynsky <54021935+zarczynsky@users.noreply.github.com>
2021-01-20 12:04:50 +0100

    cookies required

---
+++ src/middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const User = require('../schema/userSchema');
+const UnauthorizedException = require("../exceptions/unauthorized-exception");
+
+
+module.exports = ({required} = {}) => (req, res, next) => {
+    const token = req.cookies.auth
+    if (!token) throw new UnauthorizedException();
+    let decodedToken;
+
+    try {
+        decodedToken = jwt.verify(token, config.jwtSecret);
+    } catch {
+        throw new UnauthorizedException('Wrong token');
+    }
+
+    const user = {
+        _id: decodedToken.sub,
+        // role: decodedToken.role
+    }
+
+    // if ((required && !user) || (required && !~roles.indexOf(user.role))) {
+    //     throw new UnauthorizedException();
+    // }
+
+    req.user = user;
+    next();
+};
