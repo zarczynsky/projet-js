d5ce32a13ec0049281e289a7c78f3a609148b681
Jakub Szumilas <szumilas.jakub@gmail.com>
2021-01-26 04:52:17 +0100

    Update dishes.controller.js

--- src/api/dishes/dishes.controller.js
+++ src/api/dishes/dishes.controller.js
@@ -4,11 +4,37 @@ const asyncHandler = require("../async-handler");
 const auth = require('../../middlewares/auth')
 const router = new Router();
 
-// POST /api/authors
 router.post('/', asyncHandler(async (req, res) => {
-    const body = req.body;
-    const author = await Dish.create(body);
-    res.json(author.view());
+    const name = req.body.name;
+    const ingredients = req.body.ingredients;
+    const time = req.body.times;
+    const recipe = req.body.recipe;
+    const likes = req.body.likes;
+    const id = req.body.author_id
+
+    const token = req.cookies.auth // TODO: Throws "Cannot read property 'auth' of undefined"
+
+    const ai = new Dish({
+        name: name,
+        ingredients: ingredients,
+        time: time,
+        text: recipe,
+        likes: likes,
+        author_id: id
+    });
+    await ai.save(function (err) {
+        if (err) {
+            console.log(err);
+            res.json({
+                status: "Błąd bazy"
+            })
+        } else {
+            res.json({
+                status: "Przepis dodany"
+            });
+        }
+    });
+
 }))
 
 router.get('/find/name', asyncHandler(async (req, res) => {
@@ -88,37 +114,5 @@ router.post('/find/ingredients', auth({
     });
 }))
 
-router.post('/', asyncHandler(async (req, res) => {
-    const name = req.body.name;
-    const ingredients = req.body.ingredients;
-    const time = req.body.times;
-    const recipe = req.body.recipe;
-    const likes = req.body.likes;
-    const id = req.body.author_id
-
-    const token = req.cookies.auth
-
-    const ai = new Dish({
-        name: name,
-        ingredients: ingredients,
-        time: time,
-        text: recipe,
-        likes: likes,
-        author_id: id
-    });
-    await ai.save(function (err) {
-        if (err) {
-            console.log(err);
-            res.json({
-                status: "Błąd bazy"
-            })
-        } else {
-            res.json({
-                status: "Przepis dodany"
-            });
-        }
-    });
-
-}))
 
 module.exports = router
