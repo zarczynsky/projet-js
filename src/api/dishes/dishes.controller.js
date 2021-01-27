a8e86023f654927988d95a6d3171a8f6644916a2
zarczynsky <54021935+zarczynsky@users.noreply.github.com>
2021-01-27 15:30:59 +0100

    put&delete dish

--- src/api/dishes/dishes.controller.js
+++ src/api/dishes/dishes.controller.js
@@ -3,47 +3,64 @@ const Dish = require('../../models/dishSchema')
 const asyncHandler = require("../async-handler");
 const auth = require('../../middlewares/auth')
 const router = new Router();
+const User = require('../../models/userSchema')
+const DishNotFoundException = require("../../exceptions/dish-not-found-exception")
 
-router.get('/route-test/:n?', asyncHandler(async (req, res) => {
+// /api/dishes/
+
+router.get('/test/:n?', asyncHandler(async (req, res) => {
     res.send(`${req.params.n}`)
 }))
 
+// TODO: creating new dishes
 router.post('/', asyncHandler(async (req, res) => {
     const name = req.body.name;
     const ingredients = req.body.ingredients;
     const time = req.body.times;
     const recipe = req.body.recipe;
     const likes = req.body.likes;
-    const id = req.body.author_id
+    // const id = req.body.author_id
+    // const token = req.cookies.auth // TODO: Throws "Cannot read property 'auth' of undefined" 
 
-    const token = req.cookies.auth // TODO: Throws "Cannot read property 'auth' of undefined" 
-
-    const ai = new Dish({
+    const dish = new Dish({
         name: name,
         ingredients: ingredients,
         time: time,
         text: recipe,
         likes: likes,
-        author_id: id
+        // author_id: id
     });
-    await ai.save(function (err) {
+
+    await dish.save(function (err) {
         if (err) {
             console.log(err);
-            res.status(418).json({
-                status: "Błąd bazyy"
+            res.status(400).json({
+                errorMesssage: "Database error"
             })
         } else {
-            res.json({
-                status: "Przepis dodany"
-            });
+            res.status(200).json({status: "Przepis dodany"});
         }
     });
-
 }))
 
-router.get('/find/name', asyncHandler(async (req, res) => {
-    const name = req.body.name;
-    //name = req.params.n;
+router.delete('/:id', asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    const dish = await Dish.findById(id);
+    if(!dish) throw new DishNotFoundException();
+    await dish.remove();
+    res.status(204).end();
+}))
+
+router.put('/:id', asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const book = await Dish.findById(id);
+    if(!book) throw new BookNotFoundException();
+}))
+
+router.delete('/:n', asyncHandler(async (req, res) => {
+    let result = {};
+    const name = req.params.n;
+
     const dish = await Dish.findOne({
         name: name
     }, function (err, result) {
@@ -53,72 +70,71 @@ router.get('/find/name', asyncHandler(async (req, res) => {
             return (result);
         }
     });
-
-    const author_name = await User.findOne({
-        _id: dish["author_id"]
-    }, function (err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            return (result["name"]);
-        }
-    });
-
-    console.log(author_name.user_view())
-    let result = {};
-    result["author"] = author_name["name"];
     result["dish"] = dish
 
-    res.json(result);
+    // TODO: Finding author name, add to result
+    // const author_name = await User.findOne({
+    //     _id: dish["author_id"]
+    // }, function (err, result) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         return (result["name"]);
+    //     }
+    // });
+    // console.log(author_name.user_view())
+
+    // result["author"] = s // dish.author_id -> name
+    res.status(200).json(result);
 }))
 
+// router.post('/find/ingredients', auth({
+//     required: true
+// }), asyncHandler(async (req, res) => {
+//     const reqIngredients = req.body.ingredients;
+//     Dish.find({
+//         ingredients: reqIngredients
+//     }, function (err, result) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.json(result);
+//         }
+//     });
 
+// REMOVE
+// router.post('/find/name/:n', asyncHandler(async (req, res) => {
+//     const name = req.body.name;
+//     //name = req.params.n;
+//     const dish = await Dish.findOne({
+//         name: name
+//     }, function (err, result) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             return (result);
+//         }
+//     });
 
-router.post('/find/name/:n', asyncHandler(async (req, res) => {
-    const name = req.body.name;
-    //name = req.params.n;
-    const dish = await Dish.findOne({
-        name: name
-    }, function (err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            return (result);
-        }
-    });
+//     const author_name = await User.findOne({
+//         _id: dish["author_id"]
+//     }, function (err, result) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             return (result["name"]);
+//         }
+//     });
 
-    const author_name = await User.findOne({
-        _id: dish["author_id"]
-    }, function (err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            return (result["name"]);
-        }
-    });
+//     console.log(author_name.user_view())
+//     let result = {};
+//     result["author"] = author_name["name"];
+//     result["dish"] = dish
 
-    console.log(author_name.user_view())
-    let result = {};
-    result["author"] = author_name["name"];
-    result["dish"] = dish
+//     res.json(result);
+// }))
 
-    res.json(result);
-}))
-
-router.post('/find/ingredients', auth({
-    required: true
-}), asyncHandler(async (req, res) => {
-    const reqIngredients = req.body.ingredients;
-    Dish.find({
-        ingredients: reqIngredients
-    }, function (err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(result);
-        }
-    });
-}))
+// }))
 
 
 module.exports = router
